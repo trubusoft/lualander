a3fef48e4eabb563f8cd62506d31e47e87360149
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2025-10-09 12:14:30 +0700

    add Lander._rigidBody2D

--- Assets/Scripts/Lander.cs
+++ Assets/Scripts/Lander.cs
@@ -2,8 +2,10 @@ using System;
 using UnityEngine;
 
 public class Lander : MonoBehaviour {
+    private Rigidbody2D _rigidbody2D;
+    
     private void Awake() {
-        throw new NotImplementedException();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     private void Update() {
