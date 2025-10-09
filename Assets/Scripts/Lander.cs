2e5de5fd857c623f5f60f8ed1d82d23e136d35a8
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2025-10-09 10:52:51 +0700

    add Awake

--- Assets/Scripts/Lander.cs
+++ Assets/Scripts/Lander.cs
@@ -2,6 +2,10 @@ using System;
 using UnityEngine;
 
 public class Lander : MonoBehaviour {
+    private void Awake() {
+        throw new NotImplementedException();
+    }
+
     private void Update() {
         throw new NotImplementedException();
     }
