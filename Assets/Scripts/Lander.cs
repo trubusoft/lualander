cd472b557b585566406c546cb937db23145869d7
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2025-10-09 12:53:44 +0700

    update thrust and torque to use delta time; adjust linear/angular damping

--- Assets/Scripts/Lander.cs
+++ Assets/Scripts/Lander.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Lander : MonoBehaviour {
-    private const float ThrustSpeed = 5f;
-    private const float TorqueSpeed = 0.8f;
+    private const float ThrustSpeed = 700f;
+    private const float TorqueSpeed = 300f;
 
     private Rigidbody2D _rigidbody2D;
 
@@ -19,19 +19,19 @@ public class Lander : MonoBehaviour {
 
     private void HandleUpwardThrust() {
         if (Keyboard.current.upArrowKey.isPressed) {
-            _rigidbody2D.AddForce(transform.up * ThrustSpeed, ForceMode2D.Force);
+            _rigidbody2D.AddForce(transform.up * (ThrustSpeed * Time.deltaTime), ForceMode2D.Force);
         }
     }
     
     private void HandleLeftRotation() {
         if (Keyboard.current.leftArrowKey.isPressed) {
-            _rigidbody2D.AddTorque(TorqueSpeed);
+            _rigidbody2D.AddTorque(TorqueSpeed * Time.deltaTime);
         }
     }
 
     private void HandleRightRotation() {
         if (Keyboard.current.rightArrowKey.isPressed) {
-            _rigidbody2D.AddTorque(-TorqueSpeed);
+            _rigidbody2D.AddTorque(-TorqueSpeed * Time.deltaTime);
         }
     }
 }
