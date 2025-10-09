7c3a9d07c1d6b745c63a49a56c77ca96f89915c8
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2025-10-09 12:29:41 +0700

    add upward thrust to Lander

--- Assets/Scripts/Lander.cs
+++ Assets/Scripts/Lander.cs
@@ -1,14 +1,31 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Lander : MonoBehaviour {
+    private const float ThrustSpeed = 5f;
+
     private Rigidbody2D _rigidbody2D;
-    
+
     private void Awake() {
         _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
-    private void Update() {
+    private void FixedUpdate() {
+        HandleUpwardThrust();
+    }
+
+    private void HandleUpwardThrust() {
+        if (Keyboard.current.upArrowKey.isPressed) {
+            _rigidbody2D.AddForce(transform.up * ThrustSpeed, ForceMode2D.Force);
+        }
+    }
+    
+    private void HandleLeftRotation() {
+        throw new NotImplementedException();
+    }
+
+    private void HandleRightRotation() {
         throw new NotImplementedException();
     }
 }
