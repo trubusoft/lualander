e61c6a7d30bbc036f280a0533000bb6a6213c629
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2025-10-14 09:44:34 +0700

    update LandedUI.cs to show score

--- Assets/Scripts/LandedUI.cs
+++ Assets/Scripts/LandedUI.cs
@@ -4,4 +4,38 @@ using UnityEngine;
 public class LandedUI : MonoBehaviour {
     [SerializeField] TextMeshProUGUI titleTextMesh;
     [SerializeField] TextMeshProUGUI statsTextMesh;
+
+    private void Start() {
+        Lander.instance.OnLanding += LanderOnLanding;
+        Hide();
+    }
+
+    private void LanderOnLanding(object sender, Lander.OnLandingArgs e) {
+        if (e.LandingType == Lander.LandingType.Success) {
+            titleTextMesh.text = "Successful Landing";
+        } else if (e.LandingType == Lander.LandingType.LandedTooFast) {
+            titleTextMesh.text = "Landed too Fast";
+        } else if (e.LandingType == Lander.LandingType.LandedTooSteep) {
+            titleTextMesh.text = "Landed too Steep";
+        } else if (e.LandingType == Lander.LandingType.LandedOnTerrain) {
+            titleTextMesh.text = "Crashed";
+        }
+
+        var landingSpeed = Mathf.Round(e.LandingSpeed);
+        var landingAngle = Mathf.Round(e.LandingAngle);
+
+        statsTextMesh.text = $"{landingSpeed}\n" +
+                             $"{landingAngle}\n" +
+                             $"x{e.ScoreMultiplier}\n" +
+                             $"{e.Score}";
+        Show();
+    }
+
+    private void Hide() {
+        gameObject.SetActive(false);
+    }
+
+    private void Show() {
+        gameObject.SetActive(true);
+    }
 }
