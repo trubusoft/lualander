d06b720aa877c0fe68452ede58e3b1230d08ad01
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2025-10-15 08:30:37 +0700

    add camera starting point

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Level : MonoBehaviour {
     [SerializeField] private int levelNumber;
     [SerializeField] private Transform landerStartingPosition;
+    [SerializeField] private Transform cameraStartingPosition;
 
     public int GetLevelNumber() {
         return levelNumber;
@@ -10,5 +11,9 @@ public class Level : MonoBehaviour {
 
     public Vector3 GetLanderStartingPosition() {
         return landerStartingPosition.position;
+    }
+
+    public Transform GetCameraStartingPosition() {
+        return cameraStartingPosition;
     }
 }
