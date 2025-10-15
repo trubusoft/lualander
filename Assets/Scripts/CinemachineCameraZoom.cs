9b12f3365438e92395662961a562e16f75526f5f
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2025-10-15 09:06:15 +0700

    add lerp and zoom speed

--- Assets/Scripts/CinemachineCameraZoom.cs
+++ Assets/Scripts/CinemachineCameraZoom.cs
@@ -4,6 +4,7 @@ using UnityEngine.Assertions;
 
 public class CinemachineCameraZoom : MonoBehaviour {
     private const float NormalOrthographicSize = 12f;
+    private const float ZoomSpeed = 2f;
     [SerializeField] private CinemachineCamera cinemachineCamera;
     private float _targetOrthographicSize;
 
@@ -15,10 +16,12 @@ public class CinemachineCameraZoom : MonoBehaviour {
 
     private void Start() {
         Assert.IsNotNull(cinemachineCamera);
+        cinemachineCamera.Lens.OrthographicSize = NormalOrthographicSize;
     }
 
     private void Update() {
-        cinemachineCamera.Lens.OrthographicSize = _targetOrthographicSize;
+        cinemachineCamera.Lens.OrthographicSize = Mathf.Lerp(cinemachineCamera.Lens.OrthographicSize,
+            _targetOrthographicSize, Time.deltaTime * ZoomSpeed);
     }
 
     public void ResetOrthographicSize() {
