de657a9d98ec771d4f342c0fcaec2498850f901a
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2025-10-15 17:10:28 +0700

    add coin pickup sound

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,12 +1,28 @@
+using System;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour {
     [SerializeField] private AudioClip crashAudioClip;
     [SerializeField] private AudioClip sadTromboneAudioClip;
 
+    [SerializeField] private AudioClip fuelPickupAudioClip;
+    [SerializeField] private AudioClip coinPickupAudioClip;
+
     void Start() {
         Lander.instance.OnLanding += LanderOnLanding;
+        Lander.instance.OnCoinPickup += LanderOnCoinPickup;
+        // todo
+        // Lander.instance.OnFuelPickup += LanderOnFuelPickup();
     }
+
+    private void LanderOnCoinPickup(object sender, EventArgs e) {
+        AudioSource.PlayClipAtPoint(coinPickupAudioClip, Camera.main.transform.position);
+    }
+
+    // todo
+    // private void LanderOnFuelPickup(object sender, EventArgs e) {
+    //     AudioSource.PlayClipAtPoint(fuelPickupAudioClip, Camera.main.transform.position);
+    // }
 
     private void LanderOnLanding(object sender, Lander.OnLandingArgs e) {
         switch (e.LandingType) {
