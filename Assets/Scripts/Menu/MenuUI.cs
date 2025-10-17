eb5dcb3bec3bf286496bd110a161bd888042ab98
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2025-10-17 15:42:40 +0700

    update MenuUI.cs to use Scene as editor object to assign the first level

--- Assets/Scripts/Menu/MenuUI.cs
+++ Assets/Scripts/Menu/MenuUI.cs
@@ -4,9 +4,10 @@ using UnityEngine.UI;
 public class Menu : MonoBehaviour {
     [SerializeField] private Button playButton;
     [SerializeField] private Button quitButton;
+    [SerializeField] private SceneLoader.Scene firstLevel;
 
     private void Awake() {
-        playButton.onClick.AddListener(() => { SceneLoader.LoadScene(SceneLoader.Scene.Game); });
+        playButton.onClick.AddListener(() => { SceneLoader.LoadScene(firstLevel); });
         quitButton.onClick.AddListener(Application.Quit);
     }
 
