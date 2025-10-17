1c7312e5e684762b2458f934ab4e4bf39dea4861
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2025-10-17 16:51:15 +0700

    init CinemachineTracker.cs

---
+++ Assets/Scripts/Game/CinemachineTracker.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class CinemachineTracker : MonoBehaviour {
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+}
