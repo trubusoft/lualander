e83effaa18191ab42aca67ca0d9351d83471b1fa
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2025-10-20 08:57:47 +0700

    update FuelUI.cs to only update fuel bar on playing

--- Assets/Scripts/Game/UI/FuelUI.cs
+++ Assets/Scripts/Game/UI/FuelUI.cs
@@ -2,23 +2,39 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class FuelUI : MonoBehaviour {
-    [SerializeField] private GameObject landerGameObject;
+    private bool _active;
     private Image _fuelBarImage;
     private Lander _lander;
 
     private void Awake() {
         _fuelBarImage = GetComponentInChildren<Image>();
+        _active = false;
     }
 
     private void Start() {
-        _lander = landerGameObject.GetComponent<Lander>();
+        _lander = FindAnyObjectByType<Lander>();
+
+        _lander.OnStateChanged += LanderOnOnStateChanged;
     }
 
     private void Update() {
         UpdateFuelBar();
     }
 
+    private void LanderOnOnStateChanged(object sender, Lander.OnStateChangedArgs e) {
+        switch (e.State) {
+            case Lander.State.Playing:
+                _active = true;
+                break;
+            default:
+                _active = false;
+                break;
+        }
+    }
+
     private void UpdateFuelBar() {
-        _fuelBarImage.fillAmount = _lander.GetFuelNormalized();
+        if (_active) {
+            _fuelBarImage.fillAmount = _lander.GetFuelNormalized();
+        }
     }
 }
