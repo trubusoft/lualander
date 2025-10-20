369a06d18040a606aede6c2b8195d47c6dbc749d
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2025-10-20 09:22:01 +0700

    update FuelUI.cs to disable UI on gameover

--- Assets/Scripts/Game/UI/FuelUI.cs
+++ Assets/Scripts/Game/UI/FuelUI.cs
@@ -3,29 +3,26 @@ using UnityEngine.UI;
 
 public class FuelUI : MonoBehaviour {
     [SerializeField] private Image fuelBarImage;
-    private bool _active = true;
     private Lander _lander;
 
     private void Start() {
         _lander = FindAnyObjectByType<Lander>();
-        _lander.OnStateChanged += LanderOnOnStateChanged;
+        _lander.OnStateChanged += LanderOnStateChanged;
     }
 
     private void Update() {
         HandleFuelBarUpdate();
     }
 
-    private void LanderOnOnStateChanged(object sender, Lander.OnStateChangedArgs e) {
+    private void LanderOnStateChanged(object sender, Lander.OnStateChangedArgs e) {
         switch (e.State) {
             case Lander.State.GameOver:
-                _active = false;
+                gameObject.SetActive(false);
                 break;
         }
     }
 
     private void HandleFuelBarUpdate() {
-        if (_active) {
-            fuelBarImage.fillAmount = _lander.GetFuelNormalized();
-        }
+        fuelBarImage.fillAmount = _lander.GetFuelNormalized();
     }
 }
