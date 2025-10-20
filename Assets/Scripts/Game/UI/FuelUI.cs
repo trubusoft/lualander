e4d2a8c61be8410bb91252aa7877ae6a5fa98f76
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2025-10-20 09:18:36 +0700

    fix FuelUI.cs to add direct reference to fule bar image

--- Assets/Scripts/Game/UI/FuelUI.cs
+++ Assets/Scripts/Game/UI/FuelUI.cs
@@ -2,39 +2,30 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class FuelUI : MonoBehaviour {
-    private bool _active;
-    private Image _fuelBarImage;
+    [SerializeField] private Image fuelBarImage;
+    private bool _active = true;
     private Lander _lander;
-
-    private void Awake() {
-        _fuelBarImage = GetComponentInChildren<Image>();
-        _active = false;
-    }
 
     private void Start() {
         _lander = FindAnyObjectByType<Lander>();
-
         _lander.OnStateChanged += LanderOnOnStateChanged;
     }
 
     private void Update() {
-        UpdateFuelBar();
+        HandleFuelBarUpdate();
     }
 
     private void LanderOnOnStateChanged(object sender, Lander.OnStateChangedArgs e) {
         switch (e.State) {
-            case Lander.State.Playing:
-                _active = true;
-                break;
-            default:
+            case Lander.State.GameOver:
                 _active = false;
                 break;
         }
     }
 
-    private void UpdateFuelBar() {
+    private void HandleFuelBarUpdate() {
         if (_active) {
-            _fuelBarImage.fillAmount = _lander.GetFuelNormalized();
+            fuelBarImage.fillAmount = _lander.GetFuelNormalized();
         }
     }
 }
